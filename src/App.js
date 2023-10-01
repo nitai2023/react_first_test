2408d6c55a69a81a418721bbb068c3d3955b091a
admin <3251736481@qq.com>
2023-10-01 16:47:05 +0800

    feat:新建一个react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          我的第一个react应用
         </p>
         <a
           className="App-link"
