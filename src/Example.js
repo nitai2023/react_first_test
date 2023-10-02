cc65d75f3eb465ca9b73369f29f70f2d76f28924
admin <3251736481@qq.com>
2023-10-02 17:19:48 +0800

    feat:引入了一张图片

--- src/Example.js
+++ src/Example.js
@@ -1,10 +1,13 @@
 import "./Example.css"
+import logo from './logo512.png';
 function Example(){
     return (
       <div id="first_example">
+        <img src={logo} alt="图标"/>
+
         <h1 id="first_h1">我的第一个react组件</h1>
       </div>
-  
+
     )
   }
 export {Example};
