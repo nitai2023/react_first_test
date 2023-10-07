ac86723960ea27ee54c57b28e668140573c2567d
admin <3251736481@qq.com>
2023-10-07 20:44:41 +0800

    feat:实现了一个简单的点击变换颜色功能

--- src/Example.js
+++ src/Example.js
@@ -1,11 +1,17 @@
 import "./Example.css"
 import logo from './logo512.png';
+import {useRef} from 'react';
 function Example(){
+  const ref=useRef()
+  const change_color=()=>{
+  ref.current.style.color ="#" +  Math.floor(Math.random() * 16777215).toString(16);
+  console.log(ref.current.style.color)
+  }
     return (
       <div id="first_example">
         <img src={logo} alt="图标"/>
 
-        <h1 id="first_h1">我的第一个react组件</h1>
+        <h1 id="first_h1" onClick={change_color} ref={ref}>我的第一个react组件</h1>
       </div>
 
     )
