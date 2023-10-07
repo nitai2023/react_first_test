b63a3ff9e1a8e4d153f889e722e6b80ade0b4479
admin <3251736481@qq.com>
2023-10-07 10:27:28 +0800

    feat:新增列表渲染

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Example } from './Example';
-import { Person } from './person';
+import { Person,List} from './person';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Example />
     <Person></Person>
+    <List></List>
   </React.StrictMode>
 );
 
--- src/person.js
+++ src/person.js
@@ -5,6 +5,13 @@ function Person(){
         </div>
     )
 }
+const people = [
+    '凯瑟琳·约翰逊: 数学家',
+    '马里奥·莫利纳: 化学家',
+    '穆罕默德·阿卜杜勒·萨拉姆: 物理学家',
+    '珀西·莱温·朱利亚: 化学家',
+    '苏布拉马尼扬·钱德拉塞卡: 天体物理学家',
+  ];
 function Student({name}){
     return(
     <h1>
@@ -12,4 +19,11 @@ function Student({name}){
     </h1>
     )
 }
-export {Student,Person}
+function List() {
+    const listItems = people.map(person =>
+      <li>{person}</li>
+    );
+    return <ul>{listItems}</ul>;
+  }
+  
+export {Student,Person,List}
